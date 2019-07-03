efd84b7ccf72fb86d6ed3c5bdb6e7ba9dd6e23fa
Guelito1 <mflorescacravens@gmail.com>
2019-07-03 13:24:39 -0700

    still working on routes

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,13 @@ const express = require('express');
 const db = require('../models');
 const router = express.Router();
 
-router.post('/', function(req, res) {
-    // TODO: Get form data and add a new record to DB
-    // call db.pokemone.create and pass in the data from the form
-    // when the promise returns, we need to redirect to /pokemon
+router.post('/', function(req,res) {
     db.story.findOrCreate({
-      name: req.body.story
-    }).then( function(data) {
-      res.redirect('/')
+        where: {
+            title: req.body.url,
+            
+
+        }
     })
-    
-  });
+}
+)
